b94bc7d22829a0ebd123580658195bdf13006bc7
kang <506071077@qq.com>
2019-01-25 11:42:08 +0800

    修改默认头像逻辑。

--- src/app/layout/default/header/components/user.component.ts
+++ src/app/layout/default/header/components/user.component.ts
@@ -8,14 +8,14 @@ import { DA_SERVICE_TOKEN, ITokenService } from '@delon/auth';
   template: `
   <nz-dropdown nzPlacement="bottomRight">
     <div class="alain-default__nav-item d-flex align-items-center px-sm" nz-dropdown>
-      <nz-avatar [nzSrc]="settings.user.imageUrl" nzSize="small" class="mr-sm"></nz-avatar>
+      <nz-avatar [nzSrc]="avatar" nzSize="small" class="mr-sm"></nz-avatar>
       {{settings.user.lastName}}
     </div>
     <div nz-menu class="width-sm">
       <div nz-menu-item routerLink="/pro/account/center"><i nz-icon type="user" class="mr-sm"></i>
         个人中心
       </div>
-      <div nz-menu-item routerLink="/pro/account/settings"><i nz-icon type="setting" class="mr-sm"></i>
+      <div nz-menu-item routerLink="/per/set"><i nz-icon type="setting" class="mr-sm"></i>
         个人设置
       </div>
       <li nz-menu-divider></li>
@@ -27,11 +27,18 @@ import { DA_SERVICE_TOKEN, ITokenService } from '@delon/auth';
   `,
 })
 export class HeaderUserComponent {
+
+  avatar = 'https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png'; // 默认头像
+
   constructor(
     public settings: SettingsService,
     private router: Router,
     @Inject(DA_SERVICE_TOKEN) private tokenService: ITokenService,
-  ) {}
+  ) {
+    if (settings.user.imageUrl !== null && settings.user.imageUrl !== '') {
+      this.avatar = settings.user.imageUrl;
+    }
+  }
 
   logout() {
     this.tokenService.clear();
--- src/app/layout/default/sidebar/sidebar.component.ts
+++ src/app/layout/default/sidebar/sidebar.component.ts
@@ -7,8 +7,15 @@ import { SettingsService } from '@delon/theme';
   templateUrl: './sidebar.component.html',
 })
 export class SidebarComponent {
+
+  avatar = 'https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png'; // 默认头像
+
   constructor(
     public settings: SettingsService,
     public msgSrv: NzMessageService,
-  ) {}
+  ) {
+    if (settings.user.imageUrl !== null && settings.user.imageUrl !== '') {
+      this.avatar = settings.user.imageUrl;
+    }
+  }
 }
